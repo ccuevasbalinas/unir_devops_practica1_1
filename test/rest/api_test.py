0cb8e7011c664a288053a3b7c5d1de2d93b7d3a3
carloscuevas <ccuevasbalinas@gmail.com>
2024-05-01 13:01:59 +0200

    Multiply rest test

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -35,5 +35,15 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "8", "ERROR SQRT"
         )
 
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/4"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "12", "ERROR MULTIPLY"
+        )
+
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
